b715400b5cccd6a19f7d22581141585f746f0798
Daniele <daniele.cannatella@gmail.com>
2024-05-31 12:52:53 +0200

    updates scripts stats 1

--- scripts/LCC_03_stats_1.R
+++ scripts/LCC_03_stats_1.R
@@ -1,5 +1,18 @@
 # Part 3 // calculating initial stats
 
+# libraries
+
+library(here)
+library(summarytools)
+library(sf)
+library(dplyr)
+library(ggplot2)
+library(forcats)
+library(leaflet)
+library(tidyr)
+library(reshape2)
+
+
 # Create custom theme ----------------------------------------------------------
 
 theme_prd <- function() {
@@ -7,7 +20,6 @@ theme_prd <- function() {
   theme(panel.background = element_blank(),
         axis.ticks.x=element_blank(), 
         axis.ticks.y=element_blank(),
-        axis.title.y= element_blank(),
         axis.text = element_text(size=6),
         axis.title = element_text(size=8),
         legend.title = element_text(size=8, face="bold"),
@@ -19,18 +31,6 @@ theme_prd <- function() {
 
 # first calculations -----------------------------------------------------------
 ## count samples in df 
-
-library(here)
-library(summarytools)
-library(sf)
-library(dplyr)
-library(ggplot2)
-library(forcats)
-library(leaflet)
-library(tidyr)
-
-
-
 
 stmatrix <- read.csv(here("data_output/02_stmatrix.csv"))
 head(stmatrix)
@@ -120,7 +120,6 @@ ab3 <- ab2+
 
 
 ab3
-#sad
 
 
 # plot land use 1992-2015 ------------------------------------------------------
@@ -168,13 +167,11 @@ sum(lctot2015$n3)
 
 library(waffle)
 
-pal <- c("#c9b42f","#a54be0","#6cd03a","#cc5baf",
-         "#6dac46","#797bd2","#da5f22","#52b980",
-         "#d45170","#ad9144","#d35b47")
-
+pal <- c("#bd4d44","#c18e3a", "#83a442", "#965baa","#407c2e","#59ca94")
+  
 labels <- c("urban areas", "agriculture/cropland",
             "agriculture/irrigated", "grassland",
-            "forest","forest/wetland","#water")
+            "forest","forest/wetland","water")
 
   
 ggplot(lctot1992, aes(fill=y1992, values=n5))+
@@ -213,7 +210,7 @@ ggplot(lctot2015, aes(fill=y2015, values=n3))+
 
 
 
-# plot urbanization ------------------------------------------------------------
+# plot urbanization trends------------------------------------------------------
 
 # Function to transform numeric values to 0 if they are not equal to 1
 transform_values <- function(df) {
@@ -275,140 +272,4 @@ ggplot(st_urb, aes(variable, value, fill=AB)) +
         legend.background = element_rect(fill="white", colour="white"),
         legend.key.size = unit(.75,'line'))+
   scale_y_continuous(limits = c(-12000, 12000))
-
-
-
-
-
-
-cities <- subset(stmatrix, select = c(3,4))
-
-head (cities)
-
-cities_obs <- cities %>%
-  group_by(city) %>%
-  summarize(count=n(),
-            perc=round(n()/nrow(cities)*100,2)
-            )
-
-head(cities_obs)
-head(cities_df)
-
-cities_df2 <- merge(cities_df, cities_obs, by.x = "NAME_2", by.y = "city")
-
-cities_df2$pdiff <- cities_df2$perc.x-cities_df2$perc.y
-
-cities_df2
-
-cities_df2$cwaffle <- round(cities_df2$count/1000,0)
-
-cities_df2 %>%
-  rename(cities_df2, city=NAME_2)
-
-library(waffle)
-
-ggplot(cities_df2, aes(fill=NAME_2, values=cwaffle))+
-  geom_waffle(n_rows = 22, size=0.2, colour="white")+
-  scale_fill_manual(name = NULL,
-                    values = c("#c9b42f",
-                               "#a54be0",
-                               "#6cd03a",
-                               "#cc5baf",
-                               "#6dac46",
-                               "#797bd2",
-                               "#da5f22",
-                               "#52b980",
-                               "#d45170",
-                               "#ad9144",
-                               "#d35b47"))+
-  coord_equal()+
-  theme_void()
-
-
-
-grouped_cities <- aggregate(cities, by=list(cities$city), FUN=length)
-
-head(grouped_cities)
-
-# count areas proportion in shapefile ------------------------------------------
-
-head(admin)
-
-admin@data
-
-admin$area_sqkm <- round(st_area(admin) / 1000000, 2)
-
-df_shape <- as.data.frame(admin)
-
-head(df_shape)
-
-df_shape <- subset(df_shape, select = c(1,4))
-
-total <- sum(df_shape$area_sqkm)
-
-df_shape$perc <- round(df_shape$area_sqkm/total*100, 2)
-
-head(df_shape)
-
-write.csv(df_shape, "output/cities_area_perc.csv")
-
-# count above/below slm in df --------------------------------------------------
-
-head(stmatrix)
-
-slm <- subset(stmatrix, select = c(4,5))
-
-head (slm)
-
-grouped_data <- aggregate(slm, by=list(slm$city, slm$above.below.s.l.), FUN=length)
-
-sum(grouped_data$city)
-
-head(grouped_data)
-
-above <-  grouped_data[ which(grouped_data$Group.2=='above'),] 
-below <-  grouped_data[ which(grouped_data$Group.2=='below'),] 
-
-cities <- grouped_cities
-
-head(cities)
-
-cities <- subset(cities, select = c(1,3))
-
-colnames(cities)[1] <- "city"
-colnames(cities)[2] <- "total"
-
-head(cities)
-
-head(above)
-above <- subset(above, select = c(1,3))
-colnames(above)[1] <- "city"
-colnames(above)[2] <- "above"
-
-head(above)
-
-below <- subset(below, select = c(1,3))
-colnames(below)[1] <- "city"
-colnames(below)[2] <- "below"
-
-head(below)
-
-cities <- cities %>% right_join(above, by="city")
-
-head(cities)
-
-cities <- cities %>% right_join(below, by="city")
-
-head(cities)
-
-#if 0 vector, then it is correct
-cities$total-(cities$above+cities$below)
-
-cities$p_above <- round(cities$above/cities$total*100, 2)
-cities$p_below <- round(100-cities$p_above, 2) # considering NA as above
-
-head(cities)
-
-
-write.csv (cities, "output/cities_elevation.csv")
 
