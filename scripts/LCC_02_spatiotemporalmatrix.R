d1da13217ba8006d0ea1b48e7a9f1a925b3f580a
Daniele <daniele.cannatella@gmail.com>
2024-07-10 06:15:27 +0200

    updated LCC_02

--- scripts/LCC_02_spatiotemporalmatrix.R
+++ scripts/LCC_02_spatiotemporalmatrix.R
@@ -5,6 +5,7 @@
 require(tidyr)
 require(sf)
 require(terra)
+require(tidyverse)
 
 
 # 1. Import csv ----------------------------------------------------------------
@@ -42,7 +43,7 @@ coord_WGS04_sf <- st_intersection(coord_WGS04_sf, bound)
 head(coord_WGS04_sf)
 
 
-# import elevation
+## import elevation
 
 DEM <- rast("data/elevation_USDG.tif")
 DEM
@@ -62,17 +63,18 @@ coord_WGS04_sf$elev <- terra::extract(DEM, coord_WGS04_sf, na.rm = TRUE)
 
 head(coord_WGS04_sf)
 
-coord_WGS04_df <- as.data.frame(coord_WGS04_sf)
+coord_WGS04_df <- as.data.frame(coord_WGS04_sf) %>%
+  mutate(long = unlist(map(coord_WGS04_df$geometry,1)),
+         lat = unlist(map(coord_WGS04_df$geometry,2)))
 
 head(coord_WGS04_df)
 
 # 3. Cleanup and prepare spatiotemporal matrix ---------------------------------
 
-stmatrix <- coord_WGS04_df[,c(1,26,28,29,2:25)]
+stmatrix <- coord_WGS04_df[,c(1,30,31,26,29,2:25)]
 
 head(stmatrix)
 colnames(stmatrix)
-head(stmatrix)
 
 stmatrix$elev[stmatrix$elev <= 10] <- "below"
 stmatrix$elev[stmatrix$elev != "below"] <- "above"
@@ -81,10 +83,10 @@ stmatrix$elev[is.na(stmatrix$elev)] <- "above"
 head(stmatrix)
 
 stmatrix <- stmatrix %>%
-  rename(city = NAME_2) %>%
+  rename(city = NAME_1) %>%
   rename(ID2 = ID) %>%
   unpack(elev) %>%
-  select(-c(geometry,ID)) %>%
+  select(-c(ID)) %>%
   rename(ID = ID2) %>%
   rename(AB = elevation_USDG)
 
@@ -98,7 +100,7 @@ bound$area_km2 <- as.numeric(bound$area_km2/1000000)
 
 head(bound)
 
-admin_area <- as.data.frame(bound[,c(1,4)])
+admin_area <- as.data.frame(bound[,c(3,13)])
 
 admin_area
 
